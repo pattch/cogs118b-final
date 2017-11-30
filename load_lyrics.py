4a842d061036cb54516b0f63d10c1a403a2862e1
Samuel <innovatests@gmail.com>
2017-11-29 20:34:27 -0800

    Add bag of words helper method

--- load_lyrics.py
+++ load_lyrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas
 
+# Load the input file as numpy array with a list of genres and words
 def load(fname):
     genres,words = set(),set()
     with open(fname) as f:
@@ -15,9 +16,32 @@ def load(fname):
     genres,words = list(genres),list(words)
     return (dat,genres,words)
 
+# Load the input file with a one-hot representation for the genre labels
 def load_one_hot_lyrics(fname):
     dat,genres,words = load(fname)
     x,y = dat[:,1],dat[:,0]
     y = np.array(pandas.get_dummies(y))
 
     return (x,y,genres,words)
+
+# Load the input file with one-hot representation for genres and word count vectors
+def load_bag_of_words(fname):
+    x,y,genres,words = load_one_hot_lyrics(fname)
+
+    # Build dict from word -> index
+    wd = {}
+    num_words = len(words)
+    for i in range(num_words):
+        word = words[i]
+        wd[word] = i
+
+    # Calculate word counts for each of the lyrics
+    x_ = []
+    for lyric in x:
+        l = [0] * num_words
+        for word in lyric.split():
+            l[wd[word]] += 1
+        x_.append(l)
+
+    x_ = np.array(x_)
+    return (x_,y,genres,words)
