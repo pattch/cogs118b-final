ebd7b83796677cce951cc8da05dabf04a789fd6b
Samuel <innovatests@gmail.com>
2017-11-29 19:13:05 -0800

    Fix bugs in preprocessing data

--- preprocess_lyric_newlines.py
+++ preprocess_lyric_newlines.py
@@ -7,21 +7,23 @@ if len(sys.argv) > 1:
     infile = sys.argv[1]
     outfile = infile.split('.')
     outfile = outfile[0] + 'cleaned.' + outfile[1]
-    print('Processing',infile)
+print('Processing',infile)
 
 def process_lyrics(l):
     genre = l[-2].lower()
     lyrics = l[-1]
     lyrics = ' '.join([pattern.sub('',word).lower() for word in re.split('\s+',lyrics)])
     return genre + ':' + lyrics
-    return l
+    # return ':'.join(l)
 
-with open('lyrics100.csv') as f:
+with open(infile) as f:
     next(f)
-    lyrics = csv.reader(f,delimiter=',',quotechar='"')
-    lyrics = [process_lyrics(l) for l in lyrics]
+    data = csv.reader(f,delimiter=',',quotechar='"')
+    lyrics = [process_lyrics(l) for l in data]
 
 print('Finished Processing. Writing to',outfile)
+print(len(lyrics))
 with open(outfile,'w') as f:
     for l in lyrics:
-        f.write(l)
+        # print(l)
+        f.write(l + '\n')
