a2a894e5cc6d8d00859fb0020bbca3ec64fd980f
Samuel <innovatests@gmail.com>
2017-12-10 00:07:30 -0800

    Begin calculating tf-idf instead of raw word counts

--- clf_sample.py
+++ clf_sample.py
@@ -8,10 +8,6 @@ if len(sys.argv) > 1:
 
 def process_line(l):
     return [int(n) for n in l.strip().split()]
-    # dat = l.strip().split()
-    # x = dat[:dim]
-    # y = dat[dim:]
-    # return (x,y)
 
 def process_words(wl):
     return wl.strip().split()
@@ -20,18 +16,25 @@ with open(fname) as f:
     dim = int(next(f))
     words = process_words(next(f))
     dat = [process_line(l) for l in f]
-    # x,y = [],[]
-    # for l in f:
-    #     xdat,ydat = process_line(l,dimensionality)
-    #     x.append(xdat)
-    #     y.append(ydat)
 
 dat = np.array(dat)
+# Get x
 x = dat[:,:dim]
+x = np.array(x,dtype=np.float64)
+# Get y
 y = dat[:,dim:]
+
+for sample in x:
+    # Divide by the sum of word counds
+    sample /= sum(sample)
+    # Scale by the IDF vector
+    sample *= idf
+
+for i in range(x.shape[0]):
+    n = norm[i]
+    if sum(n):
+        x[i] /= n
 print(x.shape,y.shape)
-# x = np.array(x)
-# y = np.array(y)
 
 print('Fitting Classifier: Random Forest Classifier')
 clf = RFC(n_estimators=128,max_depth=50,random_state=0)
--- preprocess_lyric_wordcounts.py
+++ preprocess_lyric_wordcounts.py
@@ -3,6 +3,7 @@ import numpy as np
 from itertools import count
 from load_lyrics import load_bag_of_words as load
 from scipy import sparse
+from scipy.sparse import *
 
 fname,outfile = 'sample.txt','reducedsample.txt'
 print_statistics = False
@@ -22,6 +23,7 @@ if len(sys.argv) > 2:
 print(fname,outfile,dimensionality)
 
 # Load all of the songs
+# TODO: Support only loading songs with genres from HMM
 x,y,genres,words = load(fname)
 
 print(x)
@@ -34,16 +36,57 @@ def word_counts(x_):
         s += x_[i]
     return np.array(s).reshape((len(words),))
 
-# Calculate important word metric:
-def calculate_metric(x_):
-    return word_counts(x_)
+# 'Documents' are genres, have a |W| * |G| vector to keep track of occurences in order to calculate weighting vector
+def idf(wc,y_):
+    # Calculate IDF Vector
+    idf = np.zeros((len(words),y_.shape[0])).astype(np.dtype('bool'))
+    for i in range(wc.shape[0]):
+        sample = wc[i]
+        cat = y_[i]
+        # np.nonzero returns tuple of np arrays, first 0 access gets the first
+        # nonzero list from cat, which is the only dimension, the next 0 access gets
+        # the first (and should be only) index in cat that is nonzero
+        cat_idx = np.nonzero(cat)[0][0]
+        nonzero_word_indices = np.nonzero(sample)[0]
+        for wi in nonzero_word_indices:
+            # Mark word as present in the appropriate position
+            # Marks that a given word was present for a given category
+            idf[wi][cat_idx] = True
+    # Convert boolean values back to 1s and 0s
+    idf = idf.astype(np.dtype('int'))
+    # Number of Genres Considered
+    cats = y.shape[1]
+    # Calculate IDF here -- log of N / num categories with word
+    idf_int = np.array([np.log2(cats / sum(word_in_cat)) if sum(word_in_cat) else 0 for word_in_cat in idf])
+    return idf_int.reshape((len(words),))
 
-s = calculate_metric(x)
+def tf(w):
+    if w.nnz > 0:
+        return np.array(w / max(w))
+    return w.toarray()
+
+def calculate_tf_idf(wc,y_):
+    idf_ = idf(wc,y_)
+    for w in wc:
+        w = tf(w) * idf_
+    return wc
+
+# Calculate important word metric:
+def calculate_metric(x_,y_):
+    # Get Word Counts
+    wc = word_counts(x_)
+    tf_idf_vec = calculate_tf_idf(x_,y_)
+    print(tf_idf_vec.shape)
+    tf_idf_vec = tf_idf_vec.toarray()
+    # print(tf_idf_vec)
+    return np.array(tf_idf_vec)
+
+s = calculate_metric(x,y)
 total = sum(s)
 # Pair the counts with the indices, s is now a list of tuples
 s = list(enumerate(s))
 # Sort tuples by word count, preserves the original indices
-s = sorted(s,key= lambda tup: tup[1],reverse=True)
+s = sorted(s,key=lambda tup: sum(tup[1]),reverse=True)
 
 if print_statistics:
     count = 0
